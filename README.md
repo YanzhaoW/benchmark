39ba0a081ecebf75475f2328c25e6c694497f441
Yanzhao Wang <yannzhaow@gmail.com>
2026-03-31 01:13:35 +0200

    add resizing benchmarking

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # Benchmarking of Eigen library usage
 
 
-## Dense vs Sparse
+## Resize of Matrix or vector
+
+### MacOS (m1 pro)
 
 ```text
+Unable to determine clock rate from sysctl: hw.cpufrequency: No such file or directory
+This does not affect benchmark measurements, only the metadata output.
+***WARNING*** Failed to set thread affinity. Estimated CPU frequency may be incorrect.
+2026-03-31T01:12:15+02:00
+Running ./main
 Run on (8 X 24 MHz CPU s)
 CPU Caches:
   L1 Data 64 KiB
   L1 Instruction 128 KiB
   L2 Unified 4096 KiB (x8)
-Load Average: 3.59, 3.30, 3.21
------------------------------------------------------------------------------------------------------------
-Benchmark                                                                 Time             CPU   Iterations
------------------------------------------------------------------------------------------------------------
-Using sparse matrix/iterations:10/threads:1                          154858 ns       154200 ns           10
-Using sparse matrix (only randomization)/iterations:10/threads:1     129362 ns       129400 ns           10
-Using dense matrix/iterations:10/threads:1                        728878437 ns    728162800 ns           10
+Load Average: 2.14, 2.70, 2.92
+------------------------------------------------------------------------------------------------
+Benchmark                                                      Time             CPU   Iterations
+------------------------------------------------------------------------------------------------
+Using dense matrix resize/iterations:100000/threads:1      46686 ns        46663 ns       100000
+Using vector/iterations:100000/threads:1                   47075 ns        47060 ns       100000
 ```
--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 
 namespace
 {
-    constexpr auto n_rows = 3000;
-    constexpr auto n_cols = 3000;
-    constexpr auto nnz = 10;
-    constexpr auto nnz_col = 20;
+    constexpr auto n_rows_max = 200;
+    constexpr auto n_rows_min = 30;
+    constexpr auto n_cols_max = 200;
+    constexpr auto n_cols_min = 30;
+    constexpr auto enable_hist = false;
 
-    void dense_randomize(Eigen::MatrixXf& matrix, std::mt19937& engine)
+    void dense_randomize(auto& matrix, std::mt19937& engine)
     {
         auto dist = std::uniform_real_distribution{ 10.F, 20.F };
         for (auto col : matrix.colwise())
@@ -22,79 +23,89 @@ namespace
         }
     }
 
-    void sparse_randomize(Eigen::SparseMatrix<float>& matrix, std::mt19937& engine)
-    {
-        auto value_dist = std::uniform_real_distribution{ 10.F, 20.F };
-        matrix.setZero();
-        auto nnz_array_row_idx = std::array<int, nnz>{};
-        auto nnz_array_col_idx = std::array<int, nnz>{};
-        auto nnz_array_value = std::array<float, nnz>{};
-
-        std::ranges::sample(std::views::iota(0, n_rows), nnz_array_row_idx.begin(), nnz_array_row_idx.size(), engine);
-        std::ranges::sample(std::views::iota(0, nnz_col), nnz_array_col_idx.begin(), nnz_array_col_idx.size(), engine);
-        std::ranges::generate(nnz_array_value, [&engine, &value_dist]() -> float { return value_dist(engine); });
-
-        auto reserve_index_vec = Eigen::VectorXi{ Eigen::VectorXi::Zero(n_cols) };
-        for (auto col_idx : nnz_array_col_idx)
-        {
-            reserve_index_vec[col_idx] = nnz;
-        }
-
-        matrix.reserve(reserve_index_vec);
-        for (const auto& [row_idx, col_idx, value] :
-             std::views::zip(nnz_array_row_idx, nnz_array_col_idx, nnz_array_value))
-        {
-            matrix.insert(row_idx, col_idx) = value;
-        }
-        matrix.makeCompressed();
-    }
-
     void TestDenseMatrix(benchmark::State& state)
     {
         auto rand_dev = std::random_device{};
         auto engine = std::mt19937{ rand_dev() };
+        auto row_dist = std::uniform_int_distribution{ n_rows_min, n_rows_max };
+        auto col_dist = std::uniform_int_distribution{ n_cols_min, n_cols_max };
         // auto matrix1 = Eigen::MatrixXf{ n_rows, n_cols };
         // auto matrix2 = Eigen::MatrixXf{ n_rows, n_cols };
-        auto matrix1 = Eigen::MatrixXf{ Eigen::MatrixXf::Constant(n_rows, n_cols, 1.7F) };
-        auto matrix2 = Eigen::MatrixXf{ Eigen::MatrixXf::Constant(n_rows, n_cols, 2.7F) };
-        auto matrix3 = Eigen::MatrixXf{ Eigen::MatrixXf::Zero(n_rows, n_cols) };
+        auto matrix = Eigen::MatrixXf::Zero(n_rows_max, n_cols_max).eval();
 
+        auto hist_n_rows = std::views::iota(n_rows_min, n_rows_max + 1) |
+                           std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
+                           std::ranges::to<std::unordered_map>();
+        auto hist_n_cols = std::views::iota(n_cols_min, n_cols_max + 1) |
+                           std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
+                           std::ranges::to<std::unordered_map>();
         auto sum = 0.F;
         for (auto idx : state)
         {
-            dense_randomize(matrix1, engine);
-            dense_randomize(matrix2, engine);
-            matrix3 = matrix1 * matrix2;
-            sum += matrix3.sum();
+            const auto n_row = row_dist(engine);
+            const auto n_col = col_dist(engine);
+            if constexpr (enable_hist)
+            {
+                ++hist_n_rows[n_row];
+                ++hist_n_cols[n_col];
+            }
+            matrix.resize(n_row, n_col);
+            dense_randomize(matrix, engine);
+            sum += matrix.sum() / (n_row * n_col);
+        }
+        if constexpr (enable_hist)
+        {
+            std::println("row hist: {}", hist_n_rows);
+            std::println("col hist: {}", hist_n_rows);
         }
     }
 
-    void TestSparseMatrix(benchmark::State& state, bool only_rand = true)
+    void TestVector(benchmark::State& state)
     {
         auto rand_dev = std::random_device{};
         auto engine = std::mt19937{ rand_dev() };
+        auto row_dist = std::uniform_int_distribution{ n_rows_min, n_rows_max };
+        auto col_dist = std::uniform_int_distribution{ n_cols_min, n_cols_max };
         // auto matrix1 = Eigen::MatrixXf{ n_rows, n_cols };
         // auto matrix2 = Eigen::MatrixXf{ n_rows, n_cols };
-        auto matrix1 = Eigen::SparseMatrix<float>{ n_rows, n_cols };
-        auto matrix2 = Eigen::SparseMatrix<float>{ n_rows, n_cols };
+        auto vec = std::vector<float>{};
+        vec.resize(n_rows_max * n_cols_max, 0.F);
+
+        auto hist_n_rows = std::views::iota(n_rows_min, n_rows_max + 1) |
+                           std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
+                           std::ranges::to<std::unordered_map>();
+        auto hist_n_cols = std::views::iota(n_cols_min, n_cols_max + 1) |
+                           std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
+                           std::ranges::to<std::unordered_map>();
 
         auto sum = 0.F;
         for (auto idx : state)
         {
-            sparse_randomize(matrix1, engine);
-            sparse_randomize(matrix2, engine);
-            if (not only_rand)
+            const auto n_row = row_dist(engine);
+            const auto n_col = col_dist(engine);
+            if constexpr (enable_hist)
             {
-                sum += (matrix1 * matrix2).sum();
+                ++hist_n_rows[n_row];
+                ++hist_n_cols[n_col];
             }
+            vec.resize(n_row * n_col, 0.F);
+            auto matrix =
+                Eigen::Map<Eigen::Matrix<float, -1, -1, Eigen::ColMajor>, Eigen::Aligned>(vec.data(), n_row, n_col);
+            dense_randomize(matrix, engine);
+            sum += matrix.sum() / (n_row * n_col);
+        }
+        if constexpr (enable_hist)
+        {
+            std::println("row hist: {}", hist_n_rows);
+            std::println("col hist: {}", hist_n_rows);
         }
     }
+
 } // namespace
 
 // BENCHMARK(TestMatrix)->Threads(1)->Iterations(10000000)->Name("Using Eigen")->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(TestSparseMatrix, all, false)->Threads(1)->Iterations(10)->Name("Using sparse matrix");
-BENCHMARK_CAPTURE(TestSparseMatrix, only_rand, true)->Threads(1)->Iterations(10)->Name("Using sparse matrix (only randomization)");
-BENCHMARK(TestDenseMatrix)->Threads(1)->Iterations(10)->Name("Using dense matrix");
+BENCHMARK(TestDenseMatrix)->Threads(1)->Iterations(100000)->Name("Using dense matrix resize");
+BENCHMARK(TestVector)->Threads(1)->Iterations(100000)->Name("Using vector");
 
 auto main(int argc, char** argv) -> int
 {
