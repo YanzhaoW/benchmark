dc18d7b2160ac0e6e9e5423aa1c7f76dea51ec8c
Yanzhao Wang <yannzhaow@gmail.com>
2026-04-03 00:15:03 +0200

    update

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Benchmarking of Eigen library usage
 
-
 ## Resize of Matrix or vector
 
 ### MacOS (m1 pro)
@@ -22,4 +21,43 @@ Benchmark                                                      Time             
 ------------------------------------------------------------------------------------------------
 Using dense matrix resize/iterations:100000/threads:1      46686 ns        46663 ns       100000
 Using vector/iterations:100000/threads:1                   47075 ns        47060 ns       100000
+```
+
+### Fedora (AMD server)
+
+```text
+2026-03-31T01:18:37+02:00
+Running ./main
+Run on (96 X 4000 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x48)
+  L1 Instruction 32 KiB (x48)
+  L2 Unified 1024 KiB (x48)
+  L3 Unified 36608 KiB (x2)
+Load Average: 0.56, 0.45, 0.38
+***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incur extra overhead.
+------------------------------------------------------------------------------------------------
+Benchmark                                                      Time             CPU   Iterations
+------------------------------------------------------------------------------------------------
+Using dense matrix resize/iterations:100000/threads:1      69572 ns        69559 ns       100000
+Using vector/iterations:100000/threads:1                   77340 ns        77327 ns       100000
+```
+
+```text
+2026-03-31T01:26:02+02:00
+Running ./main
+Run on (96 X 4000 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x48)
+  L1 Instruction 32 KiB (x48)
+  L2 Unified 1024 KiB (x48)
+  L3 Unified 36608 KiB (x2)
+Load Average: 0.67, 0.53, 0.42
+***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incur extra overhead.
+-------------------------------------------------------------------------------------------------------------
+Benchmark                                                                   Time             CPU   Iterations
+-------------------------------------------------------------------------------------------------------------
+Using vector/iterations:100000/threads:1                                77211 ns        77171 ns       100000
+Using dense matrix resize/iterations:100000/threads:1                   69355 ns        69318 ns       100000
+Using dense matrix conservative resize/iterations:100000/threads:1      71817 ns        71777 ns       100000
 ```
--- main.cpp
+++ main.cpp
@@ -6,12 +6,22 @@
 #include <random>
 #include <ranges>
 
+#include <stdlib.h>
+
+int numOfHeapAllocations = 0;
+
+auto operator new(size_t size) -> void*
+{
+    numOfHeapAllocations++;
+    return malloc(size);
+}
+
 namespace
 {
-    constexpr auto n_rows_max = 200;
-    constexpr auto n_rows_min = 30;
-    constexpr auto n_cols_max = 200;
-    constexpr auto n_cols_min = 30;
+    constexpr auto n_rows_max = 8;
+    constexpr auto n_rows_min = 1;
+    constexpr auto n_cols_max = 50;
+    constexpr auto n_cols_min = 3;
     constexpr auto enable_hist = false;
 
     void dense_randomize(auto& matrix, std::mt19937& engine)
@@ -24,6 +34,45 @@ namespace
     }
 
     void TestDenseMatrix(benchmark::State& state)
+    {
+        auto rand_dev = std::random_device{};
+        auto engine = std::mt19937{ rand_dev() };
+        auto row_dist = std::uniform_int_distribution{ n_rows_min, n_rows_max };
+        auto col_dist = std::uniform_int_distribution{ n_cols_min, n_cols_max };
+        // auto matrix1 = Eigen::MatrixXf{ n_rows, n_cols };
+        // auto matrix2 = Eigen::MatrixXf{ n_rows, n_cols };
+        auto matrix = Eigen::MatrixXf::Zero(n_rows_min, n_cols_min).eval();
+
+        auto hist_n_rows = std::views::iota(n_rows_min, n_rows_max + 1) |
+                           std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
+                           std::ranges::to<std::unordered_map>();
+        auto hist_n_cols = std::views::iota(n_cols_min, n_cols_max + 1) |
+                           std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
+                           std::ranges::to<std::unordered_map>();
+        auto sum = 0.F;
+        numOfHeapAllocations = 0;
+        for (auto idx : state)
+        {
+            const auto n_row = row_dist(engine);
+            const auto n_col = col_dist(engine);
+            if constexpr (enable_hist)
+            {
+                ++hist_n_rows[n_row];
+                ++hist_n_cols[n_col];
+            }
+            matrix.resize(n_row, n_col);
+            dense_randomize(matrix, engine);
+            sum += matrix.sum() / (n_row * n_col);
+        }
+        if constexpr (enable_hist)
+        {
+            std::println("row hist: {}", hist_n_rows);
+            std::println("col hist: {}", hist_n_rows);
+        }
+        std::println("memory allocation: {}", numOfHeapAllocations);
+    }
+
+    void TestConResize(benchmark::State& state)
     {
         auto rand_dev = std::random_device{};
         auto engine = std::mt19937{ rand_dev() };
@@ -49,7 +98,7 @@ namespace
                 ++hist_n_rows[n_row];
                 ++hist_n_cols[n_col];
             }
-            matrix.resize(n_row, n_col);
+            matrix.conservativeResize(n_row, n_col);
             dense_randomize(matrix, engine);
             sum += matrix.sum() / (n_row * n_col);
         }
@@ -69,7 +118,7 @@ namespace
         // auto matrix1 = Eigen::MatrixXf{ n_rows, n_cols };
         // auto matrix2 = Eigen::MatrixXf{ n_rows, n_cols };
         auto vec = std::vector<float>{};
-        vec.resize(n_rows_max * n_cols_max, 0.F);
+        vec.resize(n_rows_min * n_cols_min, 0.F);
 
         auto hist_n_rows = std::views::iota(n_rows_min, n_rows_max + 1) |
                            std::views::transform([](auto num) { return std::pair{ num, 0U }; }) |
@@ -79,6 +128,7 @@ namespace
                            std::ranges::to<std::unordered_map>();
 
         auto sum = 0.F;
+        numOfHeapAllocations = 0;
         for (auto idx : state)
         {
             const auto n_row = row_dist(engine);
@@ -88,7 +138,7 @@ namespace
                 ++hist_n_rows[n_row];
                 ++hist_n_cols[n_col];
             }
-            vec.resize(n_row * n_col, 0.F);
+            vec.resize(n_row * n_col);
             auto matrix =
                 Eigen::Map<Eigen::Matrix<float, -1, -1, Eigen::ColMajor>, Eigen::Aligned>(vec.data(), n_row, n_col);
             dense_randomize(matrix, engine);
@@ -99,13 +149,15 @@ namespace
             std::println("row hist: {}", hist_n_rows);
             std::println("col hist: {}", hist_n_rows);
         }
+        std::println("memory allocation: {}", numOfHeapAllocations);
     }
 
 } // namespace
 
 // BENCHMARK(TestMatrix)->Threads(1)->Iterations(10000000)->Name("Using Eigen")->Unit(benchmark::kMillisecond);
-BENCHMARK(TestDenseMatrix)->Threads(1)->Iterations(100000)->Name("Using dense matrix resize");
 BENCHMARK(TestVector)->Threads(1)->Iterations(100000)->Name("Using vector");
+BENCHMARK(TestDenseMatrix)->Threads(1)->Iterations(100000)->Name("Using dense matrix resize");
+BENCHMARK(TestConResize)->Threads(1)->Iterations(100000)->Name("Using dense matrix conservative resize");
 
 auto main(int argc, char** argv) -> int
 {
